9d8b46258a4770dc7ffdc09a2138c4345ff70ad6
Jose Diaz-Gonzalez <jose.diazg@protonmail.com>
2018-11-15 22:44:23 +0000

    Finish basic functionality of the digital clock

---
+++ src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <html-clock theme="red-lcd"></html-clock>
+</template>
+
+<style lang="postcss">
+  body {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    height: 100vh;
+
+    & #App {
+      font-size: 2em;
+    }
+  }
+</style>
+
+
+<script>
+import HTMLClock from './components/HTMLClock';
+export default {
+  components: {
+    'html-clock': HTMLClock
+  }
+}
+</script>
+
---
+++ src/components/HTMLClock.vue
@@ -0,0 +1,91 @@
+<template>
+    <div class="html-clock">
+        <div class="time-container hours">{{ hours }}</div>
+        <div class="sep">:</div>
+        <div class="time-container minutes">{{ minutes }}</div>
+        <div class="sep">:</div>
+        <div class="time-container seconds">{{ seconds }}</div>
+
+    </div>
+</template>
+
+<style lang="postcss">
+  .html-clock {
+    display: grid;
+    grid-template-columns: 3fr 1fr 3fr 1fr 3fr 1.5fr;
+    justify-content: center;
+    align-items: center;
+    width: 500px;
+    height: 125px;
+  }
+
+  .html-clock > div {
+    display: flex;
+    justify-content: center;
+  }
+
+  .sep {
+    animation: blink 1s linear infinite;
+  }
+
+  @keyframes blink {
+    0%,
+    49% {
+      opacity: 1;
+    }
+
+    50%,
+    100% {
+      opacity: 0;
+    }
+  }
+
+  /* Themes */
+  .red-lcd {
+    color: red;
+  }
+
+  .green-lcd {
+    color: green;
+  }
+
+  .yellow-lcd {
+    color: yellow;
+  }
+</style>
+
+<script>
+
+  export default {
+    props: ['theme'],
+    data() {
+      return {
+        hours: '--',
+        minutes: '--',
+        seconds: '--'
+      }
+    },
+    methods: {
+      updateSeconds() {
+        this.seconds = new Date().getSeconds();
+      },
+      updateMinutes() {
+        this.minutes = new Date().getMinutes();
+      },
+      updateHours() {
+        this.hours = new Date().getHours();
+      },
+      updateTime() {
+        this.updateSeconds();
+        this.updateMinutes();
+        this.updateHours();
+      }
+
+    },
+    computed: {},
+    watch: {},
+    created() {
+      setInterval(() => this.updateTime(), 1000);
+    }
+  }
+</script>
